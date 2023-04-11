18dcd5673673459dd9fe9857295297563e059405
lab_services_student <ST10090974@vcconnect.edu.za>
2023-04-11 07:53:45 +0000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using LoginDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoginDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult VerifyLogin()
+        {
+            string username = Request.Form["txtUsername"].ToString();
+            string pass = Request.Form["txtPass"].ToString();
+
+            User hm = new User("Sean","123");
+
+            if (username.Equals(hm.Username) && pass.Equals(hm.Password))
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace LoginDemo.Models
+{
+    public class User
+    {
+        public string? SupID { get; set; }
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+
+        public User(string? supID, string? name, string? phone)
+        {
+            SupID = supID;
+            Name = name;
+            Phone = phone;
+
+
+        }
+
+        public User(string? username, string? password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
